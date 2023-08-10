a3535116a6c563588f0e4eaed09428ea0b05668c
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2023-08-11 04:20:29 +0530

    Added product add and remove from cart. Refractored cart reducer function

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -6,12 +6,33 @@ import {
   Heading,
   Text,
   Button,
+  IconButton,
+  Flex,
 } from "@chakra-ui/react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+
 import { useCart } from "../context";
 import { currency_symbols } from "../util";
 
 const ProductItem = ({ product }) => {
   const { cart, dispatch } = useCart();
+
+  let currCount = cart.find((item) => item.id === product.id)?.count;
+
+  if (!currCount) currCount = 0;
+
+  const onAdd = () =>
+    dispatch({
+      itemId: product.id,
+      type: "add",
+      itemPrice: product.price,
+    });
+
+  const onIncrement = () =>
+    dispatch({ itemId: product.id, type: "update", itemCount: currCount + 1 });
+  const onDecrement = () =>
+    dispatch({ itemId: product.id, type: "update", itemCount: currCount - 1 });
+
   return (
     <Card borderRadius={"2xl"} w={"225px"}>
       <CardBody>
@@ -28,17 +49,22 @@ const ProductItem = ({ product }) => {
           <Text fontWeight={"bold"}>{`${currency_symbols[product.currency]}${
             product.price
           }`}</Text>
-          <Button
-            onClick={() =>
-              dispatch({
-                itemId: product.id,
-                type: "add",
-                itemPrice: product.price,
-              })
-            }
-          >
-            Add to Cart
-          </Button>
+          {currCount === 0 && <Button onClick={onAdd}>Add to Cart</Button>}
+          {currCount > 0 && (
+            <Flex justify={"space-around"} align={"center"}>
+              <IconButton
+                icon={<AiOutlinePlus />}
+                onClick={onIncrement}
+                rounded={100}
+              />
+              {currCount}
+              <IconButton
+                icon={<AiOutlineMinus />}
+                onClick={onDecrement}
+                rounded={100}
+              />
+            </Flex>
+          )}
         </Stack>
       </CardBody>
     </Card>
--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,30 +1,60 @@
 import { useReducer } from "react";
 import { CartContext } from ".";
 
+// const reducer = (state, action) => {
+//   const itemIndex = state.findIndex((product) => product.id === action.itemId);
+
+//   const newState = [...state];
+
+//   switch (action.type) {
+//     case "remove":
+//       if (itemIndex === -1) return state;
+//       newState[itemIndex].count--;
+
+//       if (newState[itemIndex].count === 0)
+//         return newState.filter((product) => product.id !== action.itemId);
+//       else return newState;
+//     case "add":
+//       if (itemIndex === -1)
+//         return [
+//           ...state,
+//           { id: action.itemId, count: 1, price: action.itemPrice },
+//         ];
+
+//       return state.map((product) => {
+//         if (product.id !== action.itemId) return product;
+//         return { ...product, count: product.count + 1 };
+//       });
+//     default:
+//       return state;
+//   }
+// };
+
+const addItem = (state, action) => [
+  ...state,
+  { id: action.itemId, count: 1, price: action.itemPrice },
+];
+
+const removeItem = (state, action) =>
+  state.filter((item) => item.id !== action.itemId);
+
+const updateItem = (state, action) => {
+  if (action.itemCount === 0) return removeItem(state, action);
+  return state.map((item) => {
+    if (item.id === action.itemId) return { ...item, count: action.itemCount };
+    return item;
+  });
+};
+
 const reducer = (state, action) => {
-  const itemIndex = state.findIndex((product) => product.id === action.itemId);
-
-  const newState = [...state];
-
+  console.log(state, action);
   switch (action.type) {
-    case "remove":
-      if (itemIndex === -1) return state;
-      newState[itemIndex].count--;
-
-      if (newState[itemIndex].count === 0)
-        return newState.filter((product) => product.id !== action.itemId);
-      else return newState;
     case "add":
-      if (itemIndex === -1)
-        return [
-          ...state,
-          { id: action.itemId, count: 1, price: action.itemPrice },
-        ];
-
-      return state.map((product) => {
-        if (product.id !== action.itemId) return product;
-        return { ...product, count: product.count + 1 };
-      });
+      return addItem(state, action);
+    case "update":
+      return updateItem(state, action);
+    case "remove":
+      return removeItem(state, action);
     default:
       return state;
   }
